dbbd5503888341edeafe36a8383c1b24ed09ec5c
Maksym Kovalskyi <maxkow235@gmail.com>
2019-12-25 20:38:36 +0200

    Updates

---
+++ precache-manifest.6cadc8ba226b6400690064a8c8a1d915.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39d741cd22eff622c4773d656dfe8a9",
+    "url": "/test-react-autods/index.html"
+  },
+  {
+    "revision": "5ac30f41b24c3fa01d14",
+    "url": "/test-react-autods/static/css/2.67f0b534.chunk.css"
+  },
+  {
+    "revision": "5ac30f41b24c3fa01d14",
+    "url": "/test-react-autods/static/js/2.017c7fb6.chunk.js"
+  },
+  {
+    "revision": "4ca57b6f66f969d394ef185db56928b6",
+    "url": "/test-react-autods/static/js/2.017c7fb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bafb334047c38a770a24",
+    "url": "/test-react-autods/static/js/main.35016c9a.chunk.js"
+  },
+  {
+    "revision": "ba34319de4c6fe614937",
+    "url": "/test-react-autods/static/js/runtime-main.7f5395e2.js"
+  },
+  {
+    "revision": "c7510286e6bee7ffbf71e547f9f7bc8e",
+    "url": "/test-react-autods/static/media/file.c7510286.svg"
+  }
+]);
